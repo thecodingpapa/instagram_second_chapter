6342bf1f4c874bafa47e5b45e23d91335389e4b6
victor <victor@thecodingpapa.com>
2019-11-18 13:19:19 +0800

    #60-6 using simpleSnackBar function

--- lib/widgets/sign_up_form.dart
+++ lib/widgets/sign_up_form.dart
@@ -155,8 +155,7 @@ class _SignUpFormState extends State<SignUpForm> {
     final FirebaseUser user = result.user;
 
     if (user == null) {
-      final snackBar = SnackBar(content: Text('Please try again later!'));
-      Scaffold.of(context).showSnackBar(snackBar);
+      simpleSnackbar(context, 'Please try again later!');
     }
   }
 
