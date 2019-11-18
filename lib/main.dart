b2023fbe928a9a3014cd57fbfdceeefb5a3713d3
victor <victor@thecodingpapa.com>
2019-11-18 13:40:41 +0800

    #60-8 Firebase Auth onUserStateChange issue fix

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:instagram_thecodingpapa/constants/material_white_color.dart';
 import 'package:instagram_thecodingpapa/main_page.dart';
 import 'package:instagram_thecodingpapa/screens/auth_page.dart';
+import 'package:instagram_thecodingpapa/widgets/my_progress_indicator.dart';
 
 void main() {
   return runApp(MyApp());
 }
+
+bool isItFirstData = true;
 
 class MyApp extends StatelessWidget {
   @override
@@ -15,10 +18,15 @@ class MyApp extends StatelessWidget {
       home: StreamBuilder<FirebaseUser>(
           stream: FirebaseAuth.instance.onAuthStateChanged,
           builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return MainPage();
+            if (isItFirstData) {
+              isItFirstData = false;
+              return MyProgressIndicator();
+            } else {
+              if (snapshot.hasData) {
+                return MainPage();
+              }
+              return AuthPage();
             }
-            return AuthPage();
           }),
       theme: ThemeData(primarySwatch: white),
     );
