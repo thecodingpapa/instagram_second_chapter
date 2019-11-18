1303513495a35c978a4226f5f291c415cc9f2ea2
victor <victor@thecodingpapa.com>
2019-11-18 11:18:16 +0800

    #60-4 check if user login wz firebase and redirect

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,26 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_thecodingpapa/constants/material_white_color.dart';
 import 'package:instagram_thecodingpapa/main_page.dart';
+import 'package:instagram_thecodingpapa/screens/auth_page.dart';
 
-void main(){
+void main() {
   return runApp(MyApp());
 }
-class MyApp extends StatelessWidget{
+
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MainPage(),
-      theme: ThemeData(
-        primarySwatch: white
-      ),
+      home: StreamBuilder<FirebaseUser>(
+          stream: FirebaseAuth.instance.onAuthStateChanged,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return MainPage();
+            }
+            return AuthPage();
+          }),
+      theme: ThemeData(primarySwatch: white),
     );
   }
 }
-
-
